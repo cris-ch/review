c7ea703c6f7e08cd156370780d684aab9580e707
Cristian <cristian.christensen@gmail.com>
2022-11-02 10:58:38 +0100

    Added input validation and test for empty input

--- src/report.js
+++ src/report.js
@@ -1,24 +1,33 @@
 // report array function
 const report = (grades) => {
-  let gradesArr = grades.split(", ");
-  console.log('gradesArr: ', gradesArr);
-  let green = 0;
-  let amber = 0;
+  if (typeof grades !== "string") {
+    throw new Error("Input must be a comma-separated string");
+  }
 
+  let gradesArr = grades.split(", ");
+  let gradesCount = {};
   for (let i = 0; i < gradesArr.length; i++) {
-    if (gradesArr[i] === 'Green') {
-      green++;
-    } else if (gradesArr[i] === 'Amber') {
-      amber++;
+    if (gradesArr[i] === "Green") {
+      gradesCount.Green = gradesCount.Green + 1 || 1;
+    } else if (gradesArr[i] === "Amber") {
+      gradesCount.Amber = gradesCount.Amber + 1 || 1;
+    } else if (gradesArr[i] === "Red") {
+      gradesCount.Red = gradesCount.Red + 1 || 1;
     }
   }
-  if (amber === 0) {
-    return `Green: ${green}`;
-  } else if (green === 0) {
-    return `Amber: ${amber}`;
-  } else {
-    return `Green: ${green}\\nAmber: ${amber}`;
+
+  let gradesReport = "";
+  let counter = 1;
+  for (let key in gradesCount) {
+    if (counter === Object.keys(gradesCount).length) {
+      gradesReport += key + ": " + gradesCount[key];
+    } else {
+      gradesReport += key + ": " + gradesCount[key] + "\\n";
+    }
+    counter++;
   }
+  console.log("gradesReport: " + gradesReport);
+  return gradesReport;
 };
 
 module.exports = report;
--- test/report.test.js
+++ test/report.test.js
@@ -1,19 +1,41 @@
 const report = require("../src/report");
 
 describe("report", () => {
+  it("should throw an error: ArgumentError 'Input must be a comma-separated string' if input is not a string", () => {
+    expect(() => report(123)).toThrow("Input must be a comma-separated string");
+  });
+
+  it("should return an empty string if input is an empty string", () => {
+    expect(report("")).toBe("");
+  });
+
   it("should return Green: 1", () => {
     expect(report("Green")).toBe("Green: 1");
   });
 
   it("should return Green: 2", () => {
-    expect(report('Green, Green')).toBe("Green: 2");
+    expect(report("Green, Green")).toBe("Green: 2");
   });
 
   it("should return Green: 1\\nAmber: 1", () => {
-    expect(report('Green, Amber')).toBe("Green: 1\\nAmber: 1");
+    expect(report("Green, Amber")).toBe("Green: 1\\nAmber: 1");
   });
 
   it("should return Green: 2\\nAmber: 1", () => {
-    expect(report('Green, Green, Amber')).toBe("Green: 2\\nAmber: 1");
-  } );
-})
+    expect(report("Green, Green, Amber")).toBe("Green: 2\\nAmber: 1");
+  });
+
+  it("should return Green: 1\\nAmber: 2", () => {
+    expect(report("Green, Amber, Amber")).toBe("Green: 1\\nAmber: 2");
+  });
+
+  it("should return Green: 1\\nAmber: 1\\nRed: 1", () => {
+    expect(report("Green, Amber, Red")).toBe("Green: 1\\nAmber: 1\\nRed: 1");
+  });
+
+  it("should return Green: 2\\nAmber: 1\\nRed: 1", () => {
+    expect(report("Green, Green, Amber, Red")).toBe(
+      "Green: 2\\nAmber: 1\\nRed: 1"
+    );
+  });
+});
